55453e2c9d49710011f31d702372f75570a73462
unknown <alenclover@yandex.ru>
2023-06-14 19:59:39 +0300

    Домашнее задание 1

---
+++ task1.js
@@ -0,0 +1,26 @@
+// Первая задача
+const sum = (num1, num2)=>{
+  if(isNaN(num1)|| isNaN(num2)){
+    throw Error("Переданы некорректные данные");
+  }else{
+    result = +num1 + +num2
+    return +result.toFixed(3)
+  }
+}
+
+//Вторая задача
+function getNumberRadix(number, radix) {
+  if (typeof number === "string") {
+    number = parseInt(number);
+  }
+  if (isNaN(number) || !Number.isInteger(number) || number < 1) {
+    throw Error("Функция getNumberRadix была вызвана с некорректными параметрами");
+  }
+  if (!Number.isInteger(radix) || radix < 2 || radix > 16) {
+    throw Error("Функция getNumberRadix была вызвана с некорректными параметрами");
+  }
+  
+  return number.toString(radix);
+}
+console.log(getNumberRadix("16", 8))
+
