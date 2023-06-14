5b99d900853ba41f2584675ceea8de076e560df1
Valery Ivaniuk <leraivaniuk95@gmail.com>
2023-06-14 17:19:49 +0300

    Homework 1

---
+++ task2.js
@@ -0,0 +1,20 @@
+const validateArgFuncNumberRadix = (userNumber, userRadix) => {
+    if(
+        !(
+            Number.isInteger(userNumber) || 
+            typeof userNumber === 'string' &&
+            +userNumber > 0
+        ) || 
+        !(
+            Number.isInteger(userRadix) &&
+            userRadix >= 2 &&
+            userRadix <= 16
+        )
+    ) {
+        throw new Error ("Функция getNumberRadix была вызвана с некорректными параметрами");
+    }
+}
+function getNumberRadix(number, radix) {
+    validateArgFuncNumberRadix(number, radix);
+    return Number(number).toString(radix);
+}
