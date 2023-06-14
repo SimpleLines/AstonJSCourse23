255a6c0a5f4dc85613d5a7a8aeb1368edb72bc41
Nik G <nik.golovin83@gmail.com>
2023-06-14 16:45:25 +0600

    поменял название параметров валидирующей функции второго задания

--- task1.js
+++ task1.js
@@ -54,18 +54,18 @@ sum('20', 0.403333);
 // Подсказка: Используйте метод toString для перевода числа в другую систему
 // счисления.
 
-const validateGetNumberRadixParams = (num, rad) => {
-    if (isUndefined(num) || isUndefined(rad)) {
+const validateGetNumberRadixParams = (usersNum, usersRadix) => {
+    if (isUndefined(usersNum) || isUndefined(usersRadix)) {
         throw new Error('Функция getNumberRadix могла быть вызвана без параметров');
-    } else if(isNaN(+num)){
+    } else if(isNaN(+usersNum)){
         throw new Error('Функция getNumberRadix, в параметр num было передано не число');
-    } else if (+num < 0) {
+    } else if (+usersNum < 0) {
         throw new Error('Функция getNumberRadix, в параметр num было передано отрицательное число');
-    } else if (!Number.isInteger(+num)) {
+    } else if (!Number.isInteger(+usersNum)) {
         throw new Error('Функция getNumberRadix, в параметр num было передано не целое число');
-    } else if (isNaN(+rad)) {
+    } else if (isNaN(+usersRadix)) {
         throw new Error('Функция getNumberRadix была вызвана с некорректными параметрами, radix не число');
-    } else if (rad < 2 || rad > 16) {
+    } else if (usersRadix < 2 || usersRadix > 16) {
         throw new Error('Функция getNumberRadix вызвана с параметром radix больше 16 или меньше 2');
     }
 };
