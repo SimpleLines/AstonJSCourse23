378f2a3aabf8f09e6f2d27bb9047abfd948f3a4d
Nik G <nik.golovin83@gmail.com>
2023-06-14 14:07:53 +0600

    Поправил решение первой задачи в теле условия если строка, скопировал условие второй задачи, создал ф-ю второй задачи, вынес валидацию за тело ф-ии

--- task1.js
+++ task1.js
@@ -14,21 +14,63 @@ const isUndefined = (arg) => typeof arg === 'undefined';
 const isString = (arg) => typeof arg === 'string';
 const validateAndTransformString = (arg) => {
     const properArg = Number(arg)
-    if(isNaN(properArg)) {
+    if (isNaN(properArg)) {
         throw new Error('Некорректные данные');
-    } 
+    }
     return properArg;
-}
+};
 
 function sum(a, b) {
-    if(isUndefined(a) || isUndefined(b)) {
-        throw new Error('Не задано число в первом или втором аргументе')
-    } else if (isString(a) || isString(b)){
-        return validateAndTransformString(a) + validateAndTransformString(b)
+    if (isUndefined(a) || isUndefined(b)) {
+        throw new Error('Не задано число в первом или втором аргументе');
+    } else if (isString(a) || isString(b)) {
+        const result = validateAndTransformString(a) + validateAndTransformString(b);
+        return parseFloat(result.toFixed(3));
     } else {
         const result = a + b;
         return parseFloat(result.toFixed(3));
     }
-}
+};
 
-console.log(sum(1, 2));
+sum('20', 0.403333);
+
+// Задача 2.
+// Написать функцию getNumberRadix(number, radix), которая на вход принимает два
+// аргумента number и radix, переводит число number в систему счисления radix и
+// возвращает число в строковом представлении.
+// Number считается корректным, если:
+// - является положительным целым числом;
+// - является строкой, которую можно перевести в положительное целое число.
+// Radix считается корректным, если является числом не меньше 2 и не больше 16.
+// Если функция была вызвана с некорректными параметрами, тогда она должны
+// выбросить ошибку (throw Error(message)) c сообщением "Функция getNumberRadix
+// была вызвана с некорректными параметрами".
+// ➢ Примеры:
+// Вызываем getNumberRadix(4, 2), получаем "100"
+// Вызываем getNumberRadix("16", 8), получаем "20"
+// Вызываем getNumberRadix("Hello", 4), получаем ошибку
+// Вызываем getNumberRadix(10, 32), получаем ошибку
+// Вызываем getNumberRadix(10, "JS"), получаем ошибку
+// Подсказка: Используйте метод toString для перевода числа в другую систему
+// счисления.
+
+const validateGetNumberRadixParams = (num, rad) => {
+    if (isUndefined(num) || isUndefined(rad)) {
+        throw new Error('Функция getNumberRadix могла быть вызвана без параметров');
+    } else if (isNaN(+rad) || isNan(+num)) {
+        throw new Error('Функция getNumberRadix была вызвана с некорректными параметрами');
+    } else if (rad < 2 || rad > 16) {
+        throw new Error('Функция getNumberRadix была вызвана с некорректными параметрами');
+    }
+};
+
+function getNumberRadix(number, radix) {
+    validateGetNumberRadixParams(number, radix);
+    if (isString(number)) {
+        const properNum = Number(number);
+        return parseFloat(properNum, radix);
+    }
+    return parseInt(number, radix);
+};
+
+console.log(getNumberRadix());
