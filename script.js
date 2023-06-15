71ad0b82e2c16a060362ff15b8a687271c7f3fc1
Sergei <serjmu@mail.ru>
2023-06-15 13:41:21 +0300

    add func sum and getNumberRadix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sum = (a, b) => {
   return !isNaN(a, b)
     ? +(Number(a) + Number(b)).toFixed(3)
     : 'Не верный тип данных'
-}
+};
 
 const getNumberRadix = (number, radix) => {
  if(Number.isInteger(+number) && !isNaN(radix) && +number>0 && radix >= 2 && radix <=16) {
@@ -10,6 +10,6 @@ const getNumberRadix = (number, radix) => {
  } else {
     throw Error('Функция getNumberRadix была вызвана с некорректными параметрами');
  }
-}
+};
 
 
