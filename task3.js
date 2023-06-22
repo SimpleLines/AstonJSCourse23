7af4ad6d169fa61028ea026ecdb4c759847d6444
r2d21995250491405 <67788425+r2d21995250491405@users.noreply.github.com>
2023-06-22 19:58:14 +0500

    done3

--- task3.js
+++ task3.js
@@ -1,4 +1,29 @@
-function getUniqArray(arr) {
-    const newArr = [...new Set(arr)];
-    return newArr
+function createPerson({ name = 'New User', skills = [] }) {
+    return {
+        name,
+        skills,
+        addSkill: function (skill) {
+            this.skills.push(skill)
+            this.skills = [...new Set(this.skills)]
+            return this
+        },
+        removeSkill: function (skill) {
+            const index = this.skills.indexOf(skill)
+            if (index !== -1) {
+                this.skills.splice(index, 1);
+            }
+            return this
+        },
+        addName: function (name) {
+            this.name = name
+            return this
+        }
+    }
 }
+
+
+
+
+
+
+
