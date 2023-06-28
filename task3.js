496369babfd1b1120362630bf0469dc2687b29af
Nik G <nik.golovin83@gmail.com>
2023-06-28 15:30:16 +0600

    решил вторую задачу

--- task3.js
+++ task3.js
@@ -15,6 +15,21 @@ deleteElementFromArray([ 10, 80, 90, 30 ], 100) ;
 
 //Задача 2.
 
-function addElementsToArray(arr, index, ...elem) {
-
+const addElementsToArray = (arr, index) => (...elem) => {
+    if(!Number.isInteger(index) ||  index < 1){
+        throw new Error('the index cannot be a negative number or a fractional number')
+    }
+    if(index >= arr.length){
+       const mixedArray = [...arr, ...elem]
+       return mixedArray;
+    } else {
+        const mixedArray = [...arr.slice(0, index), ...elem, ...arr.slice(index)]
+        return mixedArray;
+    }
 }
+
+addElementsToArray([10, 80, 90, 30])(100, 200);
+addElementsToArray([10, 80, 90, 30], 2)(100, 200);
+addElementsToArray([10, 80, 90, 30], 50)(100, 200);
+addElementsToArray([10, 80, 90, 30], -2)(100, 200);
+addElementsToArray([10, 80, 90, 30], 2.5)(100, 200);
