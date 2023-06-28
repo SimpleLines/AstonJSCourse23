a942c3dc5ccd237ebded6c2a6c19d2ab7b19f59f
Nik G <nik.golovin83@gmail.com>
2023-06-28 13:34:16 +0600

    задания ко второй лекции решены

--- task2.js
+++ task2.js
@@ -1,8 +1,13 @@
+//Helpers
+const checkArrsNum = (array, functionName) => {
+    for (let el of array) {
+        if (isNaN(el)) {
+            throw new Error(`В функции ${functionName}Одно из значений не число`);
+        }
+    }
+}
+
 // Задача 1.
-// Написать функцию deepCopyObject(obj) для глубокого копирования объекта.
-// Функция должна принимать на вход объект и возвращать его копию. Запрещается
-// использовать JSON.stringify() + JSON.parse(), structuredClone() или методы
-// библиотек. Пример объекта для копирования взять из файла exampleCopyObject.js.
 
 const staff = {
     name: 'Alex',
@@ -44,26 +49,53 @@ const staff = {
     avatar: null,
 };
 
-function deepCopyObject(obj){
-
+function deepCopyObject(obj) {
+    const clone = {}
+    for(let i in obj){
+        if(obj[i] instanceof Object){
+            clone[i] = deepCopyObject(obj[i])
+            continue;
+        }
+        clone[i] = obj[i];
+    }
+    return clone;
 }
 
+const result = deepCopyObject(staff)
+console.log(staff === result)
 
-function getUniqArray(arr){
+//Задача 2
+
+function getInterval(arr, from, to) {
+
+    if (isNaN(from)) {
+        throw new Error('В функцию getInterval были переданы невалидные параметры. Параметр from должен быть числом')
+    } else if (isNaN(to)) {
+        throw new Error('В функцию getInterval были переданы невалидные параметры. Параметр to должен быть числом')
+    }
+    checkArrsNum(arr, 'getInterval');
+    const filteredArr = arr.filter((el) => from > to ? el <= from && el >= to : el >= from && el <= to)
+    return filteredArr;
+}
+
+getInterval([10, 40, 5, 15, 25], 5, 20)
+getInterval([10, 40, 5, 15, 25], 20, 5)
+getInterval([1, 2, 3, 4, 'hello'], 2, 4)
+
+//Задача 3
+function getUniqArray(arr) {
     const resultArr = [];
-    if(typeof arr === 'undefined'){
+    if (typeof arr === 'undefined') {
         throw new Error('Функция getUniqArray, нет аргумента');
     }
-    if(arr.length <=0){
+    if (arr.length <= 0) {
         throw new Error('Функция getUniqArray, пустой массив');
     }
-    for(let el of arr) {
-        if(isNaN(el)) {
-            throw new Error('Функция getUniqArray, одно из значений не число');
-        }
-    }
+    checkArrsNum(arr, 'getUniqArray');
     const result = [...new Set(arr)];
     return result;
 }
 
-console.log(getUniqArray ([10,20,5,10,20]))
+getUniqArray([10, 20, 5, 10, 20]);
+
+queueMicrotask()
