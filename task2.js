4bf21f39f33844120039b4939b70b42f2983c00a
PustynnikovKirill <kirillputynnikov1@gmail.com>
2023-07-01 17:54:08 +0300

    homework 4

--- task2.js
+++ task2.js
@@ -1,27 +1,36 @@
 function Company(name, salary) {
-    Object.defineProperty(this, 'name', { value: name, writable: false })
-    Object.defineProperty(this, 'salary', { value: salary, writable: false })
-    Object.defineProperty(this, 'income', { value: 0, writable: true })
+    this._name = name
+    this._salary = salary
 
-    Company.store.staffList.push(this)
-    Company.store.countStaff++
+    this.income = function (value) {
+        Company.store.money += value - this._salary
+        Company.store.staffList = Company.store.staffList.map(el => el.name === this._name ? {
+            ...el,
+            income: el.income + value - salary
+        } : el)
+    }
+    this.spend = function (value) {
+        Company.store.money -= value
+        Company.store.staffList = Company.store.staffList.map(el => el.name === this._name ? {
+            ...el,
+            income: el.income - value
+        } : el)
+    }
+    Company.addStaff({name, income: 0})
 }
+
 Company.store = {
     staffList: [],
     countStaff: 0,
     money: 0,
 }
-Company.addStaff = function(staff) {
-    const newStaff = new Company(staff.name, staff.salary)
-    Company.store.staffList.push(newStaff)
-    Company.store.countStaff++
+Company.addStaff = function (staff) {
+    Company.store.staffList.push(staff)
+    this.store.countStaff++
 }
-Company.getLeaders = function() {
-    const sortedStaffList = Company.store.staffList.sort((a, b) => b.income - a.income)
-    const maxIncome = sortedStaffList[0].income
+Company.getLeaders = function () {
+    const sortedStaffList = this.store.staffList.sort((a, b) => b.income - a.income)
+    const maxIncome = sortedStaffList[0]?.income
     return sortedStaffList.filter(item => item.income === maxIncome)
 }
-Company.prototype.income = function(value) {
-    Company.store.money += value - this.salary
-    this.income += value - this.salary
-}
+
