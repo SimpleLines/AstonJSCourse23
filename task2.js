bf9039e23a50ab726c899d95cabf0a85510e97d5
Sergei <serjmu@mail.ru>
2023-07-02 14:28:21 +0300

    refactor

--- task2.js
+++ task2.js
@@ -1,21 +1,3 @@
-const deepCopyObject = (obj) => {
-  const newObj = {};
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      if (typeof obj[key] === "object" && obj[key] !== null) {
-        if (obj[key] instanceof Array) {
-          newObj[key] = obj[key].slice();
-        } else if (obj[key] instanceof Date) {
-          newObj[key] = new Date(obj[key]);
-        } else {
-          newObj[key] = deepCopyObject(obj[key]);
-        }
-      } else newObj[key] = obj[key];
-    }
-  }
-  return newObj;
-};
-
 const getInterval = (arr, from, to) => {
   if (!Number.isFinite(from)) {
     throw Error(`Параметр ${from} должен быть числом`);
@@ -31,8 +13,4 @@ const getInterval = (arr, from, to) => {
   return from < to
     ? arr.filter((el) => el >= from && el <= to)
     : arr.filter((el) => el >= to && el <= from);
-};
-
-const getUniqArray = (arr) => {
-  return Array.from(new Set(arr));
 };
