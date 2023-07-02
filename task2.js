e8b671f4b947e2f58a1afe369c7bdd1a28613202
Anton <antoha_denisenkov@yandex.ru>
2023-07-02 11:21:03 +0500

    добавил 2-й файл перенес функцию

---
+++ task2.js
@@ -0,0 +1,30 @@
+function getNumberRadix(number, radix) {
+    try {
+      if (
+        !Number.isInteger(number) &&
+        (typeof number !== 'string' || !Number.isInteger(parseInt(number))) &&
+        number > 0
+      ) {
+        throw new Error(
+          'Функция getNumberRadix была вызвана с некорректными параметрами'
+        );
+      }
+      if (radix < 2 || radix > 16) {
+        throw new Error(
+          'Функция getNumberRadix была вызвана с некорректными параметрами'
+        );
+      }
+  
+      const strNumber = number.toString();
+      const newNumber = parseInt(strNumber, 10).toString(radix);
+      if (newNumber === 'NaN') {
+        throw new Error(
+          'Функция getNumberRadix была вызвана с некорректными параметрами'
+        );
+      }
+      return newNumber;
+    } catch (error) {
+      return error.message;
+    }
+  }
+  
