32a9a171ba124342fc871c7fdeac9123bc354322
Ramil Khosnetdinov <ramilkhos@gmail.com>
2023-07-02 04:18:57 +0400

    completed tasks (#83)
    Co-authored-by: Granit <1>

---
+++ task1.js
@@ -0,0 +1,9 @@
+Array.prototype.filterArray = function(cb, thisArg) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb.call(thisArg, this[i], i, this)) {
+      result.push(this[i]);
+    };
+  };
+  return result;
+};
