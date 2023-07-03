f1af789fd30f5c8b1ed96ab6763c62ffb677e1ce
Nik G <nik.golovin83@gmail.com>
2023-07-03 09:29:50 +0600

    исправил задания для третьей лекции

--- task2.js
+++ task2.js
@@ -1,100 +1,12 @@
-//Helpers
-const checkArrsNum = (array, functionName) => {
-    for (let el of array) {
-        if (isNaN(el)) {
-            throw new Error(`В функции ${functionName}Одно из значений не число`);
-        }
+const addElementsToArray = (arr, index) => (...elem) => {
+    if(!Number.isInteger(index) ||  index < 1){
+        throw new Error('the index cannot be a negative number or a fractional number')
+    }
+    if(index >= arr.length){
+       const mixedArray = [...arr, ...elem]
+       return mixedArray;
+    } else {
+        const mixedArray = [...arr.slice(0, index), ...elem, ...arr.slice(index)]
+        return mixedArray;
     }
 }
-
-// Задача 1.
-
-const staff = {
-    name: 'Alex',
-    age: 20,
-    skills: [
-        {
-            id: 1,
-            value: 'html',
-        },
-        {
-            id: 2,
-            value: 'js',
-        },
-        {
-            id: 3,
-            value: 'css',
-        },
-    ],
-    cost: undefined,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ...',
-    knowledge: [
-        {
-            label: 'React JS',
-            score: 7,
-        },
-        {
-            label: 'JS',
-            score: 7,
-        },
-        {
-            label: 'CSS',
-            score: 9,
-        },
-        {
-            label: 'HTML',
-            score: 10,
-        },
-    ],
-    avatar: null,
-};
-
-function deepCopyObject(obj) {
-    const clone = {}
-    for(let i in obj){
-        if(obj[i] instanceof Object){
-            clone[i] = deepCopyObject(obj[i])
-            continue;
-        }
-        clone[i] = obj[i];
-    }
-    return clone;
-}
-
-const result = deepCopyObject(staff)
-console.log(staff === result)
-
-//Задача 2
-
-function getInterval(arr, from, to) {
-
-    if (isNaN(from)) {
-        throw new Error('В функцию getInterval были переданы невалидные параметры. Параметр from должен быть числом')
-    } else if (isNaN(to)) {
-        throw new Error('В функцию getInterval были переданы невалидные параметры. Параметр to должен быть числом')
-    }
-    checkArrsNum(arr, 'getInterval');
-    const filteredArr = arr.filter((el) => from > to ? el <= from && el >= to : el >= from && el <= to)
-    return filteredArr;
-}
-
-getInterval([10, 40, 5, 15, 25], 5, 20)
-getInterval([10, 40, 5, 15, 25], 20, 5)
-getInterval([1, 2, 3, 4, 'hello'], 2, 4)
-
-//Задача 3
-function getUniqArray(arr) {
-    const resultArr = [];
-    if (typeof arr === 'undefined') {
-        throw new Error('Функция getUniqArray, нет аргумента');
-    }
-    if (arr.length <= 0) {
-        throw new Error('Функция getUniqArray, пустой массив');
-    }
-    checkArrsNum(arr, 'getUniqArray');
-    const result = [...new Set(arr)];
-    return result;
-}
-
-getUniqArray([10, 20, 5, 10, 20]);
-
--- task3.js
+++ task3.js
@@ -1,42 +1,3 @@
-//Задача 1.
-
-function deleteElementFromArray(arr, elem) {
-    const indxElem = arr.indexOf(elem);
-    if(indxElem){
-        const slicesArr = [...arr.slice(0, indxElem), ...arr.slice(indxElem + 1)];
-        return slicesArr;
-    } else {
-        return arr;
-    }
-}
-
-deleteElementFromArray([ 10, 80, 90, 30, 90 ], 90); 
-deleteElementFromArray([ 10, 80, 90, 30 ], 100) ;
-
-//Задача 2.
-
-const addElementsToArray = (arr, index) => (...elem) => {
-    if(!Number.isInteger(index) ||  index < 1){
-        throw new Error('the index cannot be a negative number or a fractional number')
-    }
-    if(index >= arr.length){
-       const mixedArray = [...arr, ...elem]
-       return mixedArray;
-    } else {
-        const mixedArray = [...arr.slice(0, index), ...elem, ...arr.slice(index)]
-        return mixedArray;
-    }
-}
-
-// addElementsToArray([10, 80, 90, 30])(100, 200);
-// addElementsToArray([10, 80, 90, 30], 2)(100, 200);
-// addElementsToArray([10, 80, 90, 30], 50)(100, 200);
-// addElementsToArray([10, 80, 90, 30], -2)(100, 200);
-// addElementsToArray([10, 80, 90, 30], 2.5)(100, 200);
-
-//Задача 3.
-
-
 function createPerson(settings) {
     const name = settings.name || "New User";
     const skills = settings.skills || [];
@@ -64,11 +25,3 @@ function createPerson(settings) {
     };
   }
   
-  const wizard = createPerson({name:'Harry Potter', skills:['magic', 'kindness']});
-
-  wizard.addName('Nik')
-  wizard.addSkill('Frontend developer')
-  wizard.addSkill('Frontend developer')
-  wizard.addSkill('For test')
-  wizard.removeSkill('For test')
-  console.log(wizard)
