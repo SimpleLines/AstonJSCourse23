2a87d19c2a0d1bf5190886f0221a542ff8476882
Vladimir <derekpro@mail.ru>
2023-07-03 11:05:39 +0300

    функции перенесены по разным файлам

--- task1.js
+++ task1.js
@@ -1,37 +1,14 @@
 function sum(a, b) {
-    number1 = parseFloat(a);
-    number2 = parseFloat(b);
+  number1 = parseFloat(a);
+  number2 = parseFloat(b);
 
-    if (isNaN(number1) || isNaN(number2)) {
-    return console.log('необходимо ввести 2 числа');
-    }
-  
-    let result = number1 + number2;
+  if (isNaN(number1) || isNaN(number2)) {
+    return console.log("необходимо ввести 2 числа");
+  }
 
-    result = result.toFixed(3);
-  
-    return result;
-}
+  let result = number1 + number2;
 
-function getNumberRadix(number, radix) {
-  
-    if (  
-        !(Number.isInteger(number) && number > 0) &&   
-        !(typeof number === "string" && Number.isInteger(parseInt(number)) && parseInt(number) > 0)
-    ) {   
-        throw new Error("Функция getNumberRadix была вызвана с некорректными параметрами");
-    }
- 
-    if (!(Number.isInteger(radix) && radix >= 2 && radix <= 16)) {   
-        throw new Error("Функция getNumberRadix была вызвана с некорректными параметрами"); 
-    }
+  result = result.toFixed(3);
 
-    let result; 
-    if (typeof number === "string") {   
-        result = parseInt(number).toString(radix); 
-    } else {  
-        result = number.toString(radix); 
-    }
- 
-    return result;
+  return result;
 }
