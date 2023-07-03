75f0d9ee3f53799d2b9320561288da2ca55d30b7
Nik G <nik.golovin83@gmail.com>
2023-07-03 15:40:07 +0600

    первая задача четвёртой лекции готова

--- task1.js
+++ task1.js
@@ -1,9 +1,33 @@
-function deleteElementFromArray(arr, elem) {
-    const indxElem = arr.indexOf(elem);
-    if(indxElem){
-        const slicesArr = [...arr.slice(0, indxElem), ...arr.slice(indxElem + 1)];
-        return slicesArr;
-    } else {
-        return arr;
-    }
+Array.prototype.filterArray = function(callback, thisArg) {
+    if (this === null) {
+        throw new Error("Нельзя выполнить итерацию по неопределенному или пустому значению.");
+      }
+    
+      let context = this;
+    
+      let objArr = Object(this);
+    
+      if (arguments.length > 1) {
+        context = thisArg;
+      }
+    
+      if (typeof callback !== "function") {
+        throw new Error("Переданный callback не функция");
+      }
+    
+      let len = objArr.length;
+    
+      let res = [];
+    
+      for (let i = 0; i < len; i++) {
+        if (i in objArr) {
+          let current = this[i];
+          if (callback.call(context, current, i, objArr)) {
+            res.push(current);
+          }
+        }
+      }
+    
+      return res;
 }
+
--- task2.js
+++ task2.js
@@ -1,12 +1,73 @@
-const addElementsToArray = (arr, index) => (...elem) => {
-    if(!Number.isInteger(index) ||  index < 1){
-        throw new Error('the index cannot be a negative number or a fractional number')
-    }
-    if(index >= arr.length){
-       const mixedArray = [...arr, ...elem]
-       return mixedArray;
-    } else {
-        const mixedArray = [...arr.slice(0, index), ...elem, ...arr.slice(index)]
-        return mixedArray;
-    }
+Задача 2.
+Есть функция - конструктор Company (name, salary).
+У Company есть статическое свойство store, значение которого представляет из
+себя объект и хранит в себе всю информацию о компании. Вот пример store с
+установленными значениями по умолчанию:
+{
+staffList: [], // список сотрудников
+countStaff: 0, // количество всех сотрудников
+money: 0, // общий бюджет компании
 }
+Статическое свойство/метод — это такое свойство/метод, которое доступно только
+из класса или функции конструктора из вне. То есть к статическому свойству store
+можно обратиться вот так: Company.store.
+У Company есть статический метод addStaff(staff), который добавляет нового
+сотрудника staff в конец списка сотрудников staffList объекта store. Так же всякий
+
+раз при добавлении нового сотрудника значение countStaff объекта store должно
+увеличиваться на 1.
+Аргумент staff функции addStaff(staff) представляет собой объект типа:
+{
+name, // имя сотрудника, строка
+income, // общая прибыль сотрудника, число
+}
+У Company есть статический метод getLeaders (), который возвращает массив
+сотрудников, у которых самая большая прибыль за всю историю работы в
+компании.
+Для того, чтобы создать нового сотрудника нужно вызвать
+new Company(name, salary);
+При это сотрудник представляет собой объект типа
+{
+income(value), // метод
+spend(value), // метод
+}
+где income и spend — это собственные методы объекта.
+Важно, что после создания нового объекта сотрудника не должно быть
+возможным изменять у него имя name и зарплату salary.
+метод income(value) должен увеличивать бюджет компании на value - salary
+условных единиц.
+метод spend(value) должен уменьшать бюджет компании на value условных
+единиц.
+Любое изменение бюджета, добавление сотрудника должно изменять объект
+store, чтобы он хранил в себе актуальную информацию.
+➢ Пример:
+const alex = new Company('Alex', 1000);
+const max = new Company('Max', 250);
+const peter = new Company('Peter', 250);
+const john = new Company('John', 250);
+alex.income(500);
+alex.spend(2500);
+
+max.income(500);
+peter.income(500);
+peter.income(500);
+john.income(750);
+Company.store
+/*
+результат Company.store
+{
+staffList: [
+{ name: 'Alex', income: -3000 },
+{ name: 'Max', income: 250 },
+{ name: 'Peter', income: 500 },
+{ name: 'John', income: 500 }
+],
+countStaff: 4,
+money: -1750
+}
+*/
+Company.getLeaders()
+/*
+результат вызова метода Company.getLeaders()
+[ { name: 'Peter', income: 500 }, { name: 'John', income: 500 } ]
+*/
