c842a74452b90e0044f08cc20e8c94ef0e0897e3
Nikita <tonno651@gmail.com>
2023-07-04 18:39:02 +0300

    lesson 5 done

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const SPORT_CAR = 'sport';
+const MILITARY_CAR = 'military';
+const CIVILIAN_CAR = 'civilian';
+
+const DURABILITY = 'durability';
+const LOW_FUEL_CONSUMPTION = 'lowFuelConsumption';
+const FUEL = 'fuel';
+const SPEED = 'speed';
+const POWER_REVERSE = 'powerReverse';
+
+const POINTS_NUM = 2;
+
 class Car {
   #fuel;
   #lowFuelConsumption;
   #durability;
   #speed;
 
-  #points = 2;
+  #points = POINTS_NUM;
 
   #initFuelValue = 5;
   #initSpeedValue = 10;
@@ -31,19 +43,19 @@ class Car {
     }
 
     switch (param) {
-      case 'fuel':
+      case FUEL:
         this.#fuel += 1;
         this.#calcPowerReverse();
         break;
-      case 'lowFuelConsumption':
+      case LOW_FUEL_CONSUMPTION:
         this.#lowFuelConsumption += 1;
         this.#calcPowerReverse();
         break;
-      case 'durability':
+      case DURABILITY:
         this.#durability += 1;
         this.#calcTotalDurability();
         break;
-      case 'speed':
+      case SPEED:
         this.#speed += 1;
         this.#calcTotalSpeed();
         break;
@@ -80,6 +92,46 @@ class Car {
   #calcTotalDurability() {
     this.#totalDurability = this.#initDurabilityValue + this.#durability * 0.1 * this.#initDurabilityValue;
   }
+
+  get durability() {
+    return this.#totalDurability;
+  }
+
+  get powerReverse() {
+    return this.#powerReverse;
+  }
+
+  get speed() {
+    return this.#totalSpeed;
+  }
+
+  static compare(cars) {
+    const max = {
+      [SPEED]: 0,
+      [DURABILITY]: 0,
+      [POWER_REVERSE]: 0,
+    };
+
+    cars.forEach((car) => {
+      for (const key in max) {
+        if (car[key] > max[key]) {
+          max[key] = car[key];
+        }
+      }
+    });
+
+    return cars.map((car) => {
+      const carObj = { name: car.name };
+      for (const key in max) {
+        carObj[key] = getProcent(car[key] / max[key]);
+      }
+      return carObj;
+    });
+
+    function getProcent(num) {
+      return Math.floor(num * 100) + '%';
+    }
+  }
 }
 
 class CivilianCar extends Car {
@@ -100,4 +152,65 @@ class MilitaryCar extends Car {
   }
 }
 
-const car1 = new SportCar('sport');
+function carFactory({ type, name }) {
+  let Car;
+  switch (type) {
+    case MILITARY_CAR:
+      Car = MilitaryCar;
+      break;
+    case CIVILIAN_CAR:
+      Car = CivilianCar;
+      break;
+    case SPORT_CAR:
+      Car = SportCar;
+      break;
+    default:
+      Car = CivilianCar;
+      break;
+  }
+  return new Car(name);
+}
+
+function runGame({ enemiesNumber, playerCarType, playerCarName }) {
+  const enemiesCars = [];
+  for (let i = 0; i < enemiesNumber; i++) {
+    enemiesCars.push(createRandomCar(i));
+  }
+
+  const playerCar = carFactory({ type: playerCarType, name: playerCarName });
+
+  return {
+    enemiesCars,
+    playerCar,
+  };
+}
+
+function getRandom(min, max) {
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
+
+function createRandomCar(index) {
+  const carTypes = [SPORT_CAR, CIVILIAN_CAR, MILITARY_CAR];
+  const type = getRandomParam(carTypes);
+  const name = `Car ${index + 1}`;
+  const enemyCar = carFactory({ type, name });
+  improveRandomChars(enemyCar);
+  return enemyCar;
+}
+
+function improveRandomChars(car) {
+  const chars = [DURABILITY, LOW_FUEL_CONSUMPTION, SPEED, FUEL];
+  for (let i = 0; i < POINTS_NUM; i++) {
+    const char = getRandomParam(chars);
+    car.improve(char);
+  }
+}
+
+function getRandomParam(paramsArr) {
+  const paramsNum = paramsArr.length;
+  const randomNum = getRandom(0, paramsNum - 1);
+  return paramsArr[randomNum];
+}
+
+const { enemiesCars, playerCar } = runGame({ enemiesNumber: 3, playerCarType: SPORT_CAR, playerCarName: 'Player Car' });
+Car.compare([...enemiesCars, playerCar]);
