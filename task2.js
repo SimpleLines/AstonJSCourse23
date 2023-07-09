bad35a3a739dec9a8a7a94c70e6d003369eded4d
andrey6053 <72337272+andrey6053@users.noreply.github.com>
2023-07-09 04:26:47 +0300

    HW1 (#109)
    * firstHW
    
    * HW1

---
+++ task2.js
@@ -0,0 +1,14 @@
+function validRadix(radix) {
+  return radix < 0 || radix > 16 || isNaN(radix);
+}
+
+function validNum(number) {
+  return isNaN(number) || number < 0;
+}
+
+function getNumberRadix(number, radix) {
+  let errMessage = "Функция getNumberRadix была вызвана с некорректными параметрами - ";
+  if (validRadix(radix)) throw new Error(errMessage + `${radix}`);
+  if (validNum(number)) throw new Error(errMessage + `${number}`);
+  return `${Number(number).toString(radix)}`;
+}
