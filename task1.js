8d2ffa215badd30ba19193dea053585bd151de19
Антон <113495362+Qwerander@users.noreply.github.com>
2023-07-09 05:47:50 +0500

    Denisenkov anton lesson 1 (#91)
    * task1
    
    * Добавил 2 функции по заданию
    
    * добавил 2-й файл перенес функцию
    
    * код стайл
    
    ---------
    
    Co-authored-by: Anton <antoha_denisenkov@yandex.ru>

---
+++ task1.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+  let num1 = parseFloat(a);
+  let num2 = parseFloat(b);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return null;
+  }
+
+  let result = (num1 + num2).toFixed(3);
+
+  while (result.endsWith('0') || result.endsWith('.')) {
+    result = result.slice(0, -1);
+  }
+
+  return result;
+}
