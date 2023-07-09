3a3f35f0b7126347eb5a6c71ce40a120ed49b532
lera Ivanyuk <103205215+Lera-Iv@users.noreply.github.com>
2023-07-09 03:58:59 +0300

    homework lesson 4 (#140)
    Co-authored-by: Valery Ivaniuk <leraivaniuk95@gmail.com>

---
+++ task1.js
@@ -0,0 +1,35 @@
+Array.prototype.filterArray = function (cb, thisArg) {
+    if (this == undefined) {
+        throw new Error('underfind dont iterate');
+    }
+    if (this == null) {
+        throw new Error('null dont iterate');
+    }
+    if (typeof cb !== 'function') {
+        throw new Error('callback is not a function');
+    }
+    let context = this;
+
+    if (arguments.length > 1) {
+        context = thisArg;
+    }
+
+    let obj = Object(this);
+    let len = obj.length;
+    let res = [];
+
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            let current = this[i];
+            if (cb.call(context, current, obj)) {
+                res.push(current);
+            }
+        }
+    }
+    return res;
+}
+
+
+const cb = function (item) {
+    return item >= this.min && item <= this.max;
+}
