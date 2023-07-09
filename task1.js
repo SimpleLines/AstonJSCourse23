ca6411e1a2479c533914a44d43e9282fb99b9515
Nik G <nik.golovin83@gmail.com>
2023-07-09 18:18:52 +0600

    Пагинация без кнопок, задание сделлано наполовину

--- task1.js
+++ task1.js
@@ -1,33 +1,72 @@
-Array.prototype.filterArray = function(callback, thisArg) {
-    if (this === null) {
-        throw new Error("Нельзя выполнить итерацию по неопределенному или пустому значению.");
-      }
-    
-      let context = this;
-    
-      let objArr = Object(this);
-    
-      if (arguments.length > 1) {
-        context = thisArg;
-      }
-    
-      if (typeof callback !== "function") {
-        throw new Error("Переданный callback не функция");
-      }
-    
-      let len = objArr.length;
-    
-      let res = [];
-    
-      for (let i = 0; i < len; i++) {
-        if (i in objArr) {
-          let current = this[i];
-          if (callback.call(context, current, i, objArr)) {
-            res.push(current);
-          }
-        }
-      }
-    
-      return res;
-}
+const BASE_URL = {
+    base_url: 'https://jsonplaceholder.typicode.com/',
+};
 
+const postsList = document.querySelector('.posts-list');
+const paginationList = document.querySelector('.pagination-list');
+
+
+
+
+const fetchingPosts = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        return data;
+    } catch (error) {
+        throw new Error(error)
+    }
+};
+
+
+const init = async () => {
+    let currentPage = 1;
+    const postsPerPage = 10;
+
+    const postsData = await fetchingPosts(`${BASE_URL.base_url}posts`);
+
+    
+    function displayPostList(arr, postsOnPage, currPage) {
+        const start = postsOnPage * currPage;
+        currPage--;
+        const end = start + postsOnPage;
+        const paginateArr = arr.slice(start, end);
+        paginateArr.forEach((_, i) => {
+            const postListItem = document.createElement('li');
+            const postTitle = document.createElement('h2');
+            const textSpan = document.createElement('span');
+            postsList.appendChild(postListItem);
+            postListItem.appendChild(postTitle);
+            postTitle.innerText = postsData[i].title;
+            postListItem.appendChild(textSpan);
+            postListItem.classList.add('post-item');
+            textSpan.classList.add('post-item-text');
+            textSpan.innerText = postsData[i].body;
+        })
+    };
+
+    function displayPaginationList(arr, postsOnPage) {
+        const buttonQuantity = Math.ceil(arr.length / postsOnPage);
+        for (let i = 0; i < buttonQuantity; i++) {
+            const paginationItem = document.createElement('li');
+            const paginationLink = document.createElement('a');
+            paginationLink.src = '#';
+            const textSpan = document.createElement('span');
+            paginationList.appendChild(paginationItem);
+            paginationItem.classList.add('pagination-item');
+            paginationItem.appendChild(paginationLink);
+            paginationLink.classList.add('pagination-link');
+            paginationLink.appendChild(textSpan);
+            textSpan.classList.add('pagination-text');
+            textSpan.innerText = `${i + 1}`;
+            paginationItem.addEventListener('click', () => {
+                displayPostList(postsData, postsPerPage, i);
+            })
+        }
+    };
+
+
+    displayPostList(postsData, postsPerPage, currentPage);
+    displayPaginationList(postsData, postsPerPage);
+}
+init();
