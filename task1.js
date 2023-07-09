b657864b3efa9ff9d1354c25707e4482c1152aa0
lera Ivanyuk <103205215+Lera-Iv@users.noreply.github.com>
2023-07-09 04:01:19 +0300

    homework lesson 1 (#136)
    Co-authored-by: Valery Ivaniuk <leraivaniuk95@gmail.com>

---
+++ task1.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error ('Аргументы не являются числами!');
+    };
+
+    let result = (a + b).toFixed(3);
+    
+    return parseFloat(result);
+}
